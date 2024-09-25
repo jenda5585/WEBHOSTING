08d737757ce84b24cb052098576f9d2047409c40
lords world <lordsworldeu@gmail.com>
2024-09-26 00:36:06 +0200

    update8

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Lords-World", /*Server name*/
-        serverIp: "mc.lords-world.eu", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "rabbit.hostify.cz:54700", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1282040186880131112" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
